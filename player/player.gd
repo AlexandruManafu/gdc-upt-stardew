e6e57ed16d89bc9676e1f48942e464d8a60684ce
AlexandruManafu <alexandru.manafu@protonmail.com>
2026-06-13 16:03:14 +0300

    session 1  tilemap, session 2 player move and tools

--- player/player.gd
+++ player/player.gd
@@ -1,22 +1,83 @@
 extends CharacterBody2D
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var tools_animation: AnimatedSprite2D = $ToolsAnimatedSprite2d
 
-
-@export var _speed: int = 300;
+@export var _speed: int = 100;
+var walk_speed = 100.0
+var run_speed = 200
 var _direction: Vector2 = Vector2.ZERO
-var face_direction = 1
+var press_x: bool
+var press_z: bool
+var face_direction = 1:
+	set(value):
+		face_direction = value
+		animated_sprite_2d.flip_h = face_direction == -1;
+		tools_animation.flip_h = face_direction == -1;
+		
+var is_running = false
+var press_shift = false;
+var selected_action = Tools.dig;
+var mouse_up = false;
+var mouse_down = false;
+var click = false;
+var walkable_anims = ["idle", "walk", "run"];
+var can_move = true;
 
+#region BUILT-IN
 func _ready() -> void:
 	pass
 
-
 func _physics_process(delta: float) -> void:
-	_direction = Input.get_vector("left", "right", "up", "down")
-	velocity = _direction * _speed;
+	if(!can_move): return;
+	player_move()
+	move_and_slide()
 	
-	if _direction != Vector2.ZERO:
-		animated_sprite_2d.flip_h = _direction.x == -1;
-		animated_sprite_2d.play("walk")
+func _process(delta: float) -> void:
+	if(!can_move): return;
+	player_animate()
+
+func _input(event: InputEvent) -> void:
+	_direction = Input.get_vector("left", "right", "up", "down")
+	press_x = Input.is_action_just_pressed("press_x")
+	press_z = Input.is_action_just_pressed("press_z")
+	press_shift = Input.is_action_pressed("press_shift")
+	mouse_up = Input.is_action_just_pressed("mouse_wheel_up");
+	mouse_down = Input.is_action_just_pressed("mouse_wheel_down");
+	if(mouse_up || mouse_down):
+		switch_tool();
+	click = Input.is_action_just_pressed("click");
+
+#endregion
+
+func player_move() -> void:
+	is_running = press_shift
+	_speed = run_speed if is_running else walk_speed
+	velocity = _direction * _speed;
+
+func player_animate()->void:
+	if(_direction.x != 0): face_direction = _direction.x
+	if _direction != Vector2.ZERO && walkable_anims.has(animated_sprite_2d.animation):
+		var anim = "run" if is_running else "walk"
+		animated_sprite_2d.play(anim)
+		tools_animation.play(anim)
+	elif(click):
+		can_move = false;
+		animated_sprite_2d.play(Tools.keys()[selected_action])
+		tools_animation.play(Tools.keys()[selected_action])
+		await animated_sprite_2d.animation_finished
+		await tools_animation.animation_finished
+		can_move = true;
 	else:
 		animated_sprite_2d.play("idle")
-	move_and_slide()
+		tools_animation.play("idle")
+		
+func switch_tool()-> void:
+	if(mouse_up):
+		selected_action = min(Tools.keys().size() - 1, selected_action + 1)
+	elif(mouse_down):
+		selected_action = max(0, selected_action - 1)
+		
+enum Tools{
+	dig,
+	water
+}
